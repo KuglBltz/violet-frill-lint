ee3e550d90af3dde1a3523888c2096303890f889
Glitch (violet-frill-lint) <none>
2022-02-28 14:04:28 +0000

    📶🪃 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+This is your site JavaScript code - you can add interactivity and carry out processing
+- Initially the JS writes a message to the console, and moves a button you can add from the README
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+
+/* 
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the "Next steps" in the README
+*/
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function () {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
+const btn1 = document.querySelector("contact");
+if (btn1) {
+  btn1.onclick = function () {
+    btn1.classlist.toggle("jump");
+  };
+}
+// This is a single line JS comment
+/*
+This is a comment that can span multiple lines 
+- use comments to make your own notes!
+*/
+
+// This stuff is about the navigation bar
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
